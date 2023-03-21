7ab726c70b2c4bd7e1548d40c53313f6370de881
rafficksm2k <128378827+rafficksm2k@users.noreply.github.com>
2023-03-21 10:57:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
+import React, { useReducer } from 'react';
 import Container from './scripts/container/container';
 import './styles/store.css';
 
+import reducer from './scripts/reducers/storeReducer';
+
+export const StoreContext = React.createContext();
+
 function App() {
+
+  const initValue = {
+    pageSize: 10,
+    products: [],
+    filteredProduct: [],
+    tableViewProducts: [],
+    header: [],
+    showTable: false,
+    editTable: false,
+    selectedProduct: {},
+    currentPage: 1,
+    tableSize: 0
+  };
+
+  const [store, dispatch] = useReducer(reducer, initValue);
+
   return (
-    <div className="App">
-      <Container />
+    <StoreContext.Provider value={{store, dispatch}}>
+      <div className="App">
+        <Container />
+        {/* {store.pageSize} */}
       </div>
+    </StoreContext.Provider>
   );
 }
 
